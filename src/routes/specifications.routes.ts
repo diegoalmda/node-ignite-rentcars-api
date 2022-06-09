911a11691a913bbfca1f3d5c4138a7b3206591ce
Diego Almeida <diegoalmda@gmail.com>
2022-06-09 16:18:54 -0300

    Create exceptions treatments

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 const specificationsRoutes = Router();
 
 const createSpecificationContoller = new CreateSpecificationController();
+
+specificationsRoutes.use(ensureAuthenticated);
 
 specificationsRoutes.post("/", createSpecificationContoller.handle);
 
