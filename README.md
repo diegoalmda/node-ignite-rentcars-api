8148d3b06ee690cafda3a7a3f0134f87905537e6
Diego Almeida <diegoalmda@gmail.com>
2022-06-13 12:21:31 -0300

    Implemented ensureAdmin middleware

--- README.md
+++ README.md
@@ -2,11 +2,9 @@
 
 ** Requisitos funcionais
 - Deve ser possível cadastrar um novo carro.
-- Deve ser possível listar todas as categorias.
 
 ** Regras de negócio
 - Não deve ser possível cadastrar um carro com uma placa já existente.
-- Não deve ser possível alterar a plca de um carro já cadastrado.
 - O carro deve ser cadastrado como disponível por padrão.
 - O usuário responsável pelo cadastro deve ser um usuário administrador.
 
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
 
 import { CreateSpecificationController } from "../../../../modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const specificationsRoutes = Router();
 
 const createSpecificationContoller = new CreateSpecificationController();
 
-specificationsRoutes.use(ensureAuthenticated);
-
-specificationsRoutes.post("/", createSpecificationContoller.handle);
+specificationsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationContoller.handle
+);
 
 export { specificationsRoutes };
