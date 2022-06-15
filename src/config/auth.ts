01630202dcf2bf04b10d802e582fe430e3be9191
Diego Almeida <diegoalmda@gmail.com>
2022-06-15 19:15:59 -0300

    Started creating refresh token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "92d55008e1c52040a60f489699c398bb",
+  expires_in_token: "15m",
+  secret_refresh_token: "691a7970698b34ebeef8889e296f9693",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
